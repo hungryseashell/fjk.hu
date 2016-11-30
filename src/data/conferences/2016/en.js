c5a6895d8121f9e24602f372aee909f6f41a02c7
Gabor Dobrei <gabor.dobrei@gmail.com>
2016-11-30 06:54:27 +0100

    feat(conf 2016): add speakers and seminars

--- src/data/conferences/2016/en.js
+++ src/data/conferences/2016/en.js
@@ -43,8 +43,56 @@ module.exports = {
         {
           img: '/img/doug-batchelor.jpg',
           name: 'Doug Batchelor',
-          description: 'Lelkész, média misszionárius',
-          bio: 'Doug Batchelor az Amazing Facts szervezet elnöke és előadója, valamint a kaliforniai Granite Bay Adventista gyülekezet lelkésze. Bibliai témájú TV műsora minden nap látható a amerikai kábeltelevíziós csatornákon, illetve műhold segítségével az egész világon. Házigazdája a Bible Answers Live (Bibliai válaszok Élőben) betelefonálós rádióprogramnak. Ez az adás minden vasárnap este hallgatható Észak-Amerika egész területén. Számos cikk és könyv szerzője. Doug a mély lelkiség és látás embere, mégis gyakorlatias hozzáállása és spontán humora révén képes arra, hogy keresztények és nem hívő közönséghez egyaránt szóljon. Szeret repülni, búvárkodni, quadot vezetni és gitározni. Feleségével, Karennel öt gyermekük van.'
+          description: 'Pastor',
+          bio: 'Doug Batchelor is the president and speaker for Amazing Facts Ministries and the senior pastor of the Granite Bay, California Adventist Church.  His biblical teaching programs can be seen daily on national television cable networks and globally by satellite.  He also hosts Bible Answers Live call-in radio program for Bible questions heard every Sunday night across North America. He is the author of numerous articles and books. Doug is a man of vision and deep spirituality, yet his down-to-earth approach and spontaneous humor enable him to communicate with both Christian and secular audiences.  He enjoys flying, scuba diving, riding quads and playing guitar.  He and his wife, Karen have five children.'
+        },
+        {
+          img: '/img/jan-harry-cabungcal.jpg',
+          name: 'Jan-Harry Cabungcal',
+          description: 'Evangelism coordinator, neuroscientist',
+          bio: 'Jan-Harry Cabungcal is a neuroscientist by profession, but soul-winning is his life passion. As the founder and evangelism coordinator of Europe 4 Jesus and president-elect of ASI Switzerland, Dr. Cabungcal works to inspire, equip and mobilize young people in seeking the lost, reviving the weary and pointing them to his Lord - Jesus Christ.'
+        },
+        {
+          img: '/img/csizmadia-robert.png',
+          name: 'Róbert Csizmadia',
+          description: 'Pastor',
+          bio: 'Róbert Csizmadia, pastor theologian. He obtained his MA degree in Theological Studies at Newbold College, England; he also studied there pastoral care and relationship counselling. In addition, he was the  Assistant Dean of Newbold College. He is the co-founder of the Ketten Együtt Házassági Műhely (Marital and Premarital Counselling). He worked as a lecturer at the Adventist Theological Seminary, the Janus Pannonius University of Pécs and the Károli Gáspár University of the Reformed Church in Hungary. He is happily married: his wife, Heli is from Finland, they have two daughters: Aino and Helmi. He works as a pastor of the SDA church at the moment.'
+        },
+        {
+          img: '/img/horvath-gabor.jpg',
+          name: 'Gábor Horváth',
+          description: 'Pastor, music instructor',
+          bio: 'My name is Gábor Horváth. I come from a village called Nágocs, located in the Somogy county with my wife and two sons. My original profession is a music teacher (I got my diploma as a percussion instrument teacher in Debrecen and got my master’s degree in Pécs), but God led me to realize, that beyond my love for percussion instruments I can to more to others, so I was called in 1999 to study at the Sola Scriptura Theological School. I am working as a supporting pastor and a pastor since 2002, and I was also called to be a youth leader for 8 years. During my service I have finished an addiction consultant training at Semmelweis University and since 2006 I regularly work among alcohol, drug and medicine addicts. Our residential rehabilitation program started in 2010, where as a professional leader I try to look over the work done there (find out more at <a href="http://www.alkoholbeteg.hu/" target="_blank">www.alkoholbeteg.hu</a>). In the last 10 years, I have visited many primary and secondary schools as well as camps, and I have made connections with many young people, in order to think together about the questions of addiction and breaking free. I have joined the Seventh - day Adventist church this September and continue my service as a pastor in the area of Balatonlelle.'
+        },
+        {
+          img: '/img/toth-szilard.jpg',
+          name: 'Szilárd Tóth',
+          description: 'Pastor',
+          bio: 'I am grateful to God that He called me when I was young to be a member of the family that is waiting for Him to come again; I have been serving Him as a pastor there for fifteen years. I have a special interest in the Old Testament and the Jewish culture. In my freetime, I like cycling and running.'
+        },
+        {
+          img: '/img/danko-ferenc.jpg',
+          name: 'Ferenc Dankó',
+          description: 'Pastor',
+          bio: 'I have been serving as a pastor for seven years.In spite of the changes, in this period of time there was a constant emphasis on the youth work. In my commitment to this, my own way of life, personal experiences from my adulthood and youth had and have an important role. It is an inspiring challenge as a family man to stay young, make progress, and strengthen my faith at the same time.I have learnt the most from the youth about how faithfulness and credibility is equally important.'
+        },
+        {
+          img: '/img/szabo-attila.jpg',
+          name: 'Attila Szabó',
+          description: 'Pastor, mission worker',
+          bio: 'I was born in 1956, and currently live in Győr. I was teaching all age groups as a biology and art teacher for twenty years. I got my second degree at the Sola Scriptura theological college in 1993, and from that time, I was teaching Bible and Fine arts class until 2014. Since 1996, I am working as a pastor and missionary in God’s work. My wife, Zsuzsanna Horváth is also my coworker. I used to organize reading camps for young people (ages 14-20). Lately, in the past seven years we were organising scout camps for children (ages 7-14) with our coworkers. Also, in about the last 15 years, I was leading lifestyle camps. Since 1988, I am holding meetings and series mostly in western Hungary about topics like the Bible, healthy lifestyle, creation and evolution, child and youth rearing, and marriage care. I have summed up my researches and experiences in nine books. The goal of my work with the youth is to strengthen the boys and girls in their mission and vocation, „That our sons may be as plants grown up in their youth; that our daughters may be as corner stones, polished after the similitude of a palace” (Psalms 144:12).'
+        },
+        {
+          img: '/img/ocsai-imre.jpg',
+          name: 'Imre Ócsai',
+          description: 'Pastor',
+          bio: 'I’ve been a Gospel worker for seven years. After my studies in Theology I’ve started to work at the Bible and Lifestyle Camp in Bózsva. Together with my wife and our four children we work for the church in Szolnok. I have spent most part of my life and ministry outside the Seventh-day Adventist Church. A recently discovered treasure from the Scripture and the Testimonies gave a new meaning, opened a new dimension for my ministry. New light shone upon well known truths before my eyes. These discoveries played a key role in my joining to the World Church. I hope that by sharing these I may help young people, who desire to work for Christ.'
+        },
+        {
+          img: '/img/erdodi-peter.jpg',
+          name: 'Péter Erdődi',
+          description: 'Software developer, lay Bible student',
+          bio: 'After his studies in IT, he studied Theology at the Sola Scriptura College. Together with his former classmates, his passion is Bible research, to understand and share the Gospel of Jesus Christ. Father of one currently, but soon of three children, together with his wife, Erika, they are members of the Seventh-day Adventist Church in Ráckeve.'
         }
       ]
     },
@@ -52,9 +100,77 @@ module.exports = {
       title: 'Seminars',
       seminars: [
         {
-          title: 'Bibliai kérdések és válaszok',
+          title: 'Bible Answers Live!',
           speaker: 'Doug Batchelor',
-          description: 'Szembesültél már olyan kérdésekkel a Bibliából, amire nem tudtál válaszolni? Foglalkoztat egy kérdés, és szeretnéd tudni, mi Isten Igéjének a válasza rá? <br> Doug Batchelor a Bible Answers Live! - bibliai kérdésekkel foglalkozó rádióműsor házigazdája. Most Magyarországon először, személyesen tehetjük fel a kérdéseinket, és kereshetjük együtt a Biblia pontos válaszát.'
+          description: 'Have you ever faced Bible Questions you could not answer? Are there questions in your life your life that you would like to get answered solely from the Bible?<br>Doug Batchelor, host of the Bible Answers Live! - programme is visiting Hungary for the first time! You can ask your questions, come and seek the Bible\'s true answer together.'
+        },
+        {
+          title: 'The greatest want of this generation and how this involves the youth of the Seventh-day Adventist movement<br>At the foot of the cross: Gaining Decisions',
+          speaker: 'Jan-Harry Cabungcal',
+          description: 'Do we live in a world in need of more young men of great intelligence and sophistication? What is the greatest want and need of this generation? How can the youth of the SDA movement be the catalyst and the element needed in this last generation. Can we be the generation that ends the world? This are the questions that we will tackle during this workshop and provide practical applications on reaching: higher than highest human thoughts can reach is Gods ideal for His children.<br>The bible teaches that we are not only hearers of the word of God but doers. Yet so often, after we share a critical truth with a friend or family member, no decision is made. How can we cooperate with the Holy Spirit in such a way as to gain decisions from many a soul in the valley of decision? How do we make our appeals with power, conviction and confidence? Come and learn how to bring people to the feet of Jesus, and allow the power of the cross for the individual to choose an unhindered life in these last days.'
+        },
+        {
+          title: 'Freedom in Christ',
+          speaker: 'Róbert Csizmadia',
+          description: '<div class="text-center" style="padding-bottom:10px">„If the Son therefore shall make you free, ye shall be free indeed” - (KJV) John 8:36</div>Redemption does not only provide a possible access to eternal life but it has an impact on our present life as well. This life-experience can be summarised in the phrase „Freedom in Christ”. We are going to explore and examine together this topic with the help of the Bible. How can we characterise this freedom? Being free from something or free to do something? Freedom with boundaries or unrestricted freedom? Or are these the right questions to ask at all?'
+        },
+        {
+          title: 'From addiction to mercy',
+          speaker: 'Gábor Horváth',
+          description: `I am convinced that there are two types of people. One group consists of addicts, the other group consists of those, who deny it. In my opinion, addiction goes together with existence, and through our bondages can we get to the state when we realise that we need a Saviour. I would like to share this process, my personal experiences and the realizations of my service among addicts, and I will also talk about what we can to others in our environment.
+      </p>
+
+      <p>
+        The covered topics of the seminar will be:
+      </p>
+
+      <p>
+        <ol>
+          <li><div>
+            <p>Addiction as our universal human state</p>
+            <p>- or what does my life depend on, and what kind of everyday traps should I avoid?</p>
+          </div></li>
+
+          <li><div>
+            <p>The process of the addiction’s development in the life of all of us.</p>
+            <p>- or how can I offset my tension and injuries as a child, an adolescent and an adult?</p>
+          </div></li>
+
+          <li><div>
+            <p>Addiction’s appearance and fulfillment through the pages of the Bible</p>
+            <p>Sad and uplifting examples from the Word of God about the destruction of sin and the power of grace. How can I break free from my addictions?</p>
+          </div></li>
+
+          <li><div>
+            <p>Religious addiction vs. Freedom earned in Christ</p>
+            <p>How can I live continuously my sobriety and why is it essential to pass on what I have received?</p>
+        </div></li>
+        </ol>`
+        },
+        {
+          title: 'Timetravelling – is it possible?',
+          speaker: 'Szilárd Tóth',
+          description: 'In our fast-paced world, in the midst of the rush all of us feel that we need a pause. But when, where and how? Let’s have a look at the Biblical chronology and feasts! Let’s get acquainted with their origins and significance! Let’s talk about what keeping the Sabbath means for us and what we can do to make this day a real feast!'
+        },
+        {
+          title: '„Jesus for the youth.” How to find and keep the youth living in the 21st century with the gospel?',
+          speaker: 'Ferenc Dankó',
+          description: 'Without generalizing, anyone who tried to lead young people to God have experienced, that youth work is a great challenge, whether the target group are youth who go to church or those, who live in the world. In the seminars, we try to find an answer to how can we give the gospel to the youth of our generation in a way that they can understand and live by it. What general and specific traits does the work for this generation’s youth have or can have?'
+        },
+        {
+          title: 'Where is my place, what is my duty? Mission or struggling in the net of youth? (1Jn 2,14)<br>What is the secret of a spiritual life? How will the tree of your life become fruitful? (Jer. 17,5–6)',
+          speaker: 'Attila Szabó',
+          description: 'Where is my place, what is my duty? Mission or struggling in the net of youth? (1Jn 2,14) Destruction or building? The pitfalls of a successful life based on the novel ‘A Colobre’ by Dino Buzatti. If everything seems beautiful from the outside, what is the problem eventually? Why do you live and what are you running from? What does you life talk about: Jesus, or Colombre? Pictures of God and distortions of them. <br>What is the secret of a spiritual life? How will the tree of your life become fruitful? (Jer. 17,5–6) Jesus wants to plant you from the desert next to the river. How can it happen? Where does the invisible roots and branches of your life reach? The secret of growing your root hairs in the direction of the river. What is the bole? Your flowers: the smell of life or the smell of death? Change from the roots - bearing fruits. „My roots will reach to the water, and the dew will lie all night on my branches.” (Jób 29,19)'
+        },
+        {
+          title: 'GLOW - sermon in your pocket. In mission every day.',
+          speaker: 'Enikő Szőllősi - Anna Müller',
+          description: 'God is calling us to be His missionaries, and bring the light to the world, to others - what He has given us. There are many mission methods, but there is one that you can use in almost every situation.<br>How can such small things set big local churches on  re? How can you gain experiences that you only dreamt of?<br>Come and be part of the GLOW story.'
+        },
+        {
+          title: 'Church organization - on earth as it is in heaven...?',
+          speaker: 'Péter Erdődi - Imre Ócsai',
+          description: 'What is the church? Who has invented the organization and what is its„raison d’etre”? Do I really need this anyway? Isn’t it sufficient to be connected to Christ only? Does it have anything to do with my salvation? Can we find the everlasting Gospel in the Seventh-day Adventist doctrine of the church? If you have ever asked any of these questions above then you are welcome to our interactive seminar, where we’ll find answers together from the Bible and the Spirit of Prophecy, that „the manifold wisdom of God might be known through the church” (Eph 3:10).'
         }
       ]
     },
