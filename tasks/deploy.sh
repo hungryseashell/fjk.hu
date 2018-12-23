8be1b78ef8bc3b86dca4eb377daaa9891e0a7993
gabordobrei <gabor.dobrei@gmail.com>
2018-12-23 19:48:59 +0100

    Prevent deploy w/o commit

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if git status | grep modified:; then
+  echo "You have uncommited changes..."
+  exit 1
+fi
+
 npm i
 
 COMMIT_MSG="$(git log -1 --pretty=oneline) --skip-ci"
@@ -8,22 +13,23 @@ git checkout gh-pages
 
 cp -R ./_site/* .
 
- rm -rf src
- rm -rf _temp
- rm -rf node_modules
- rm -rf bower_components
- rm -rf _site
- rm -rf tasks
- rm bower.json
- rm Gruntfile.js
- rm package.json
- rm README.md
- rm npm-debug.log
- rm package-lock.json
+rm -rf src
+rm -rf _temp
+rm -rf node_modules
+rm -rf bower_components
+rm -rf _site
+rm -rf tasks
+rm bower.json
+rm Gruntfile.js
+rm package.json
+rm README.md
+rm npm-debug.log
+rm package-lock.json
 
- git add -A
- git commit --allow-empty -am "$COMMIT_MSG"
- git push origin gh-pages
+git add -A
+git commit --allow-empty -am "$COMMIT_MSG"
+git push origin gh-pages
 
 git checkout simple-site
 npm i
+
