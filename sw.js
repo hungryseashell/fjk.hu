cf98830d973305d1a91a27d6d5fdaa09af54f330
gabordobrei <gabor.dobrei@gmail.com>
2019-01-23 15:35:37 +0100

    5cb403798cf3a9d7ace62eb5a48a249a01e99763 Run build on deploy --skip-ci

---
+++ sw.js
@@ -0,0 +1,155 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "assets/css/fjk.min.css",
+    "revision": "e0f38af48be7b480d00bd51b048d1cd1"
+  },
+  {
+    "url": "assets/img/favicon.ico",
+    "revision": "b723e79aa0304f845f1d8a451e9d2798"
+  },
+  {
+    "url": "assets/img/speaker-mask.svg",
+    "revision": "6d91e20e722bf164968d81741d5c87a4"
+  },
+  {
+    "url": "assets/img/spinner.gif",
+    "revision": "45f422ceb51df978a7f3911441881303"
+  },
+  {
+    "url": "assets/js/bootstrap-validator.min.js",
+    "revision": "40c93ea04f47fdb4526cd3ef91738900"
+  },
+  {
+    "url": "assets/js/bootstrap.min.js",
+    "revision": "1d8f1cb047c691d5adeb0e4e5744b619"
+  },
+  {
+    "url": "assets/js/fjk.min.js",
+    "revision": "15cac6645d1d6048813b5427678f4cfb"
+  },
+  {
+    "url": "assets/js/jquery.easing.min.js",
+    "revision": "019ec7c16f4b4885960fee1efea80f43"
+  },
+  {
+    "url": "assets/js/jquery.min.js",
+    "revision": "a09e13ee94d51c524b7e2a728c7d4039"
+  },
+  {
+    "url": "assets/js/jwplayer/jwplayer.js",
+    "revision": "15bf22747b8d285171c559a646e04684"
+  },
+  {
+    "url": "assets/js/snap.svg.min.js",
+    "revision": "b92f25bc9c56c69a481d2db4620f8304"
+  },
+  {
+    "url": "assets/js/wow.min.js",
+    "revision": "a26a117ff59c944bbb654bf506f69786"
+  },
+  {
+    "url": "assets/img/asi-logo.png",
+    "revision": "e163386d91fec96598fb920243a84199"
+  },
+  {
+    "url": "assets/img/bg-2019.jpg",
+    "revision": "c0adaaeebcf9750e3e24fb4210902d7c"
+  },
+  {
+    "url": "assets/img/gyulkep-2.jpg",
+    "revision": "f843f766008c6123fe9f0790bcbe080b"
+  },
+  {
+    "url": "assets/img/gyulkep-3.jpg",
+    "revision": "0fd87fd026e09b32b1554d4d9fbbeddf"
+  },
+  {
+    "url": "assets/img/gyulkep.jpg",
+    "revision": "7c9feec554ed6ff7a8d497a9368f91ba"
+  },
+  {
+    "url": "assets/img/horvath-gabor.jpg",
+    "revision": "162983db53adc04426ecaa3fb01ea07a"
+  },
+  {
+    "url": "assets/img/jonathan-ostrowski.jpg",
+    "revision": "ada6b43938537e65a2b875f6139358d7"
+  },
+  {
+    "url": "assets/img/logo-black.png",
+    "revision": "b0d8d1e54982274aa2e3b56cb14c9a59"
+  },
+  {
+    "url": "assets/img/logo-color-white.png",
+    "revision": "95a913193bdfacf3d26ebf7650089849"
+  },
+  {
+    "url": "assets/img/logo-text.png",
+    "revision": "f34a8517a48f94c00a71eb8e93f61c77"
+  },
+  {
+    "url": "assets/img/logo-white.png",
+    "revision": "1ab1d070694933dfb72a274b0f880a43"
+  },
+  {
+    "url": "assets/img/logo.png",
+    "revision": "d59e688664a2a653af4d90199248ca33"
+  },
+  {
+    "url": "assets/img/milan-moskala.jpg",
+    "revision": "6f7f33de42f05f8d73bf107f6a808104"
+  },
+  {
+    "url": "assets/img/ramos.jpg",
+    "revision": "991411317f30971448af4c154cee9219"
+  },
+  {
+    "url": "assets/img/rhema_1.jpg",
+    "revision": "739216e324596f40213ab552edf8b868"
+  },
+  {
+    "url": "assets/img/rhema_2.jpg",
+    "revision": "65607cd3fce838a0302af4838de404fd"
+  },
+  {
+    "url": "assets/img/rhema_3.jpg",
+    "revision": "e8f8e086bf71d824fa98a0e59786eb94"
+  },
+  {
+    "url": "assets/img/sda-logo.png",
+    "revision": "22d117103c505d54a00ffb597efae820"
+  },
+  {
+    "url": "booklet/2019/index.html",
+    "revision": "e950d7a554ca8f9143288a5a4c621e36"
+  },
+  {
+    "url": "en/booklet/2019/index.html",
+    "revision": "62136fc76975e892f15a632661df8a12"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
